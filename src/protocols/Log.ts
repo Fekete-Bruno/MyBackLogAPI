629aa8e1be8729a6bc4cd564dec2fc5187f4f4ab
Fekete-Bruno <brunohffekete@gmail.com>
2022-11-08 14:41:22 -0300

    feat: created Log type

---
+++ src/protocols/Log.ts
@@ -0,0 +1,10 @@
+export type Log = {
+    id?: number,
+    name: string,
+    image?:string,
+    platform:string,
+    genre:string,
+    status?:string,
+    rating?:number
+    review?:string
+} 
